7ae56baff455a35a16980c2ba97d831be4de6b28
hgi4657 <hgi4657@naver.com>
2024-07-19 20:00:09 +0900

    git init

---
+++ src/main/java/com/sparta/studytrek/StudyTrekApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.studytrek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTrekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyTrekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/studytrek/StudyTrekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.studytrek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTrekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
