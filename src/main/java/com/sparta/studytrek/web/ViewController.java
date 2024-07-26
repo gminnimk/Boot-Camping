22a1d5755bfb3ddaf5a42d81bb542645c22c40f9
DooDoo <160572286+Mua2@users.noreply.github.com>
2024-07-26 16:19:28 +0900

    🐛 bugfix : url 수정

--- src/main/java/com/sparta/studytrek/web/ViewController.java
+++ src/main/java/com/sparta/studytrek/web/ViewController.java
@@ -11,7 +11,7 @@ public class ViewController {
         return "login"; // 로그인&회원가입
     }
 
-    @GetMapping("/home")
+    @GetMapping("/api/home")
     public String showHome() {
         return "home";
     }
