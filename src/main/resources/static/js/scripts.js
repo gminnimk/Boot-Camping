d25b73a1a8d51e9ce257e78a1e7837a4aed33cb8
DooDoo <160572286+Mua2@users.noreply.github.com>
2024-07-26 11:15:30 +0900

    🐛 bugfix  : 버그 수정

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,3 +1,4 @@
+// 테마 토글 함수
 function toggleTheme() {
     const body = document.body;
     const toggleCircle = document.querySelector('.toggle-circle');
@@ -28,6 +29,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }
 });
 
+// 사이드바 토글 함수
 function toggleSidebar() {
     const sidebar = document.querySelector('.sidebar');
     const content = document.querySelector('.content');
@@ -43,12 +45,7 @@ function toggleSidebar() {
     }
 }
 
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    toggleSidebar();
-    checkLoginStatus();
-});
-
+// 페이지 로드 시 로그인 상태를 체크하여 버튼을 업데이트하는 함수
 function checkLoginStatus() {
     const loginButton = document.querySelector('.add-task-button');
     const accessToken = localStorage.getItem('accessToken');
@@ -61,3 +58,24 @@ function checkLoginStatus() {
         loginButton.onclick = () => location.href = '/api/auth';
     }
 }
+
+// 로그아웃 성공 시 호출되는 함수
+function onLogoutSuccess() {
+    const loginButton = document.querySelector('.add-task-button');
+    loginButton.textContent = 'Login';
+    loginButton.onclick = () => location.href = '/api/auth';
+    alert('로그아웃 성공');
+}
+
+// 로그아웃 요청을 보내는 함수
+function onLogout() {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    onLogoutSuccess();
+}
+
+// DOMContentLoaded 이벤트 리스너에 로그인 상태 체크 함수 추가
+document.addEventListener('DOMContentLoaded', (event) => {
+    toggleSidebar();
+    checkLoginStatus(); // 로그인 상태를 체크하는 함수 호출
+});
