4d5d4432613d1bcf8e0cb9e53c581c9bde9cf538
hgi4657 <hgi4657@naver.com>
2024-07-31 00:09:20 +0900

    ✨ update - #32 모집글 entity 필드 추가

--- src/main/java/com/sparta/studytrek/domain/recruitment/entity/Recruitment.java
+++ src/main/java/com/sparta/studytrek/domain/recruitment/entity/Recruitment.java
@@ -2,6 +2,7 @@ package com.sparta.studytrek.domain.recruitment.entity;
 
 import com.sparta.studytrek.common.Timestamped;
 import jakarta.persistence.*;
+import java.time.LocalDate;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,15 +19,35 @@ public class Recruitment extends Timestamped {
     private String title;  // 모집글 제목
 
     @Column(nullable = false)
+    private String process; // 과정 소개
+
+    @Column(nullable = false)
     private String content;  // 모집글 내용
 
     @Column(nullable = false, length = 150)
     private String place;  // 캠프 장소
 
     @Column(nullable = false)
-    private int cost;  // 캠프 비용
+    private String cost;  // 캠프 비용
 
     @Column(nullable = false, length = 100)
     private String subject;  // 캠프 주제
 
+    @Column(nullable = false)
+    private String level; // 난이도
+
+    @Column(nullable = false)
+    private String classTime;  // 수업시간
+
+    @Column(nullable = false)
+    private LocalDate campStart;  // 캠프 시작일
+
+    @Column(nullable = false)
+    private LocalDate campEnd;  // 캠프 종료일
+
+    @Column(nullable = false)
+    private LocalDate recruitStart; // 모집 시작일
+
+    @Column(nullable = false)
+    private LocalDate recruitEnd;  // 모집 종료일
 }
