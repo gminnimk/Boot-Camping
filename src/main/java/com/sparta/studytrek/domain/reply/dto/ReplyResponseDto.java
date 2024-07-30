73357a23a7db1468b4ba75b87a92dc513a9cf7c6
hgi4657 <hgi4657@naver.com>
2024-07-30 22:26:54 +0900

    🩹 fix - #40 id 도 같이 나오게 변경

--- src/main/java/com/sparta/studytrek/domain/reply/dto/ReplyResponseDto.java
+++ src/main/java/com/sparta/studytrek/domain/reply/dto/ReplyResponseDto.java
@@ -8,12 +8,14 @@ import lombok.Getter;
 
 @Getter
 public class ReplyResponseDto {
+    private Long id;
     private String content;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
     @Builder
     public ReplyResponseDto(ReviewReply reviewReply) {
+        this.id = reviewReply.getId();
         this.content = reviewReply.getContent();
         this.createdAt = reviewReply.getCreatedAt();
         this.updatedAt = reviewReply.getUpdatedAt();
