3357f80e152babebf4cc3c1e4ed7c806da0e443e
hgi4657 <hgi4657@naver.com>
2024-07-31 14:13:56 +0900

    🩹 fix - #6 병합 오류

--- src/main/java/com/sparta/studytrek/common/exception/ErrorCode.java
+++ src/main/java/com/sparta/studytrek/common/exception/ErrorCode.java
@@ -44,17 +44,17 @@ public enum ErrorCode {
 
     // recruitment
     NOTFOUND_RECRUITMENT(HttpStatus.BAD_REQUEST,"해당 모집글은 존재하지 않습니다."),
-    RECRUITMENT_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "해당 모집글을 작성한 사용자가 아닙니다.");
+    RECRUITMENT_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "해당 모집글을 작성한 사용자가 아닙니다."),
 
     // Comment
     NOTFOUND_REVIEW_COMMENT(HttpStatus.BAD_REQUEST, "해당 댓글은 존재하지 않습니다."),
 
     // Reply
-    NOTFOUND_REVIEW_REPLY(HttpStatus.BAD_REQUEST, "해당 대댓글은 존재하지 않습니다.");
+    NOTFOUND_REVIEW_REPLY(HttpStatus.BAD_REQUEST, "해당 대댓글은 존재하지 않습니다."),
 
     // Profile
     NOTFOUND_PROFILE(HttpStatus.BAD_REQUEST, "해당 프로필을 찾을 수 없습니다."),
-    PROFILE_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "프로필을 수정할 권한이 없습니다.");
+    PROFILE_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "프로필을 수정할 권한이 없습니다."),
 
     // Study
     STUDY_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 스터디 모집글은 존재하지 않습니다."),
