18ab3a726e3a15dca74ac12414d33b6932c35115
gminnimk <tvt1245@naver.com>
2024-07-31 14:52:48 +0900

    ✨ update : #41 스터디 댓글 Repository findByStudyId, findByIdAndStudyId 메서드 생성

--- src/main/java/com/sparta/studytrek/domain/comment/repository/StudyCommentRepository.java
+++ src/main/java/com/sparta/studytrek/domain/comment/repository/StudyCommentRepository.java
@@ -4,6 +4,13 @@ import com.sparta.studytrek.domain.comment.entity.StudyComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface StudyCommentRepository extends JpaRepository<StudyComment, Long> {
+
+    List<StudyComment> findByStudyId(Long studyId);
+
+    Optional<StudyComment> findByIdAndStudyId(Long commentId, Long studyId);
 }
