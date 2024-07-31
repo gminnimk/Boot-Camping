65c0db9d00210f0933e61d4633ef2b5816810c49
dlalwn <annie45671@gmail.com>
2024-07-31 23:28:53 +0900

    ✨ update: #42 답변 http 추가

--- src/main/java/com/sparta/studytrek/domain/answer/dto/AnswerRequestDto.java
+++ src/main/java/com/sparta/studytrek/domain/answer/dto/AnswerRequestDto.java
@@ -1,11 +1,15 @@
 package com.sparta.studytrek.domain.answer.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnswerRequestDto {
 
     @NotBlank(message = "답변은 필수 입력입니다.")
