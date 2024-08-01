1031fdcb71fa04acd71e6583136d06bbcfff128a
hgi4657 <hgi4657@naver.com>
2024-08-02 01:17:57 +0900

    💄 style - #61 auth 불필요한 import 삭제 및 주석,개행 정리

--- src/main/java/com/sparta/studytrek/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/sparta/studytrek/domain/auth/repository/RefreshTokenRepository.java
@@ -1,10 +1,9 @@
 package com.sparta.studytrek.domain.auth.repository;
 
 import com.sparta.studytrek.domain.auth.entity.RefreshToken;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
--- src/main/java/com/sparta/studytrek/domain/auth/repository/RoleRepository.java
+++ src/main/java/com/sparta/studytrek/domain/auth/repository/RoleRepository.java
@@ -2,10 +2,9 @@ package com.sparta.studytrek.domain.auth.repository;
 
 import com.sparta.studytrek.domain.auth.entity.Role;
 import com.sparta.studytrek.domain.auth.entity.UserRoleEnum;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
