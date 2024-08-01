4a8bf2bb7a0c33079098e30ef654301aabb9f01b
dlalwn <annie45671@gmail.com>
2024-08-01 15:25:03 +0900

    ✨ update: #30 responsedto에 id 추가

--- src/main/java/com/sparta/studytrek/domain/question/dto/QuestionResponseDto.java
+++ src/main/java/com/sparta/studytrek/domain/question/dto/QuestionResponseDto.java
@@ -7,12 +7,14 @@ import lombok.Getter;
 @Getter
 public class QuestionResponseDto {
 
+    private Long id;
     private String title;
     private String content;
     private String category;
     private LocalDateTime createdAt;
 
     public QuestionResponseDto(Question createQuestion) {
+        this.id = createQuestion.getId();
         this.title = createQuestion.getTitle();
         this.content = createQuestion.getContent();
         this.category = createQuestion.getCategory();
