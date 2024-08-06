7dff3345357b62827c2063d4bd7a3a1fffdd32f7
gminnimk <tvt1245@naver.com>
2024-08-06 19:11:15 +0900

    💄 style : #66 주석 해제

--- src/main/java/com/sparta/studytrek/domain/rank/controler/RankController.java
+++ src/main/java/com/sparta/studytrek/domain/rank/controler/RankController.java
@@ -1,27 +1,27 @@
-//package com.sparta.studytrek.domain.rank.controler;
-//
-//import com.sparta.studytrek.domain.rank.dto.RankResponseDto;
-//import com.sparta.studytrek.domain.rank.dto.FilterRequest;
-//import com.sparta.studytrek.domain.rank.service.RankService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/ranks")
-//public class RankController {
-//
-//    @Autowired
-//    private RankService rankService;
-//
-//    @GetMapping("/filter")
-//    public List<RankResponseDto> filterRanks(
-//        @RequestParam(required = false) List<String> track,
-//        @RequestParam(required = false) List<String> environment,
-//        @RequestParam(required = false) List<String> cost) {
-//
-//        FilterRequest filterRequest = new FilterRequest(track, environment, cost);
-//        return rankService.getFilteredRanks(filterRequest);
-//    }
-//}
+package com.sparta.studytrek.domain.rank.controler;
+
+import com.sparta.studytrek.domain.rank.dto.RankResponseDto;
+import com.sparta.studytrek.domain.rank.dto.FilterRequest;
+import com.sparta.studytrek.domain.rank.service.RankService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ranks")
+public class RankController {
+
+    @Autowired
+    private RankService rankService;
+
+    @GetMapping("/filter")
+    public List<RankResponseDto> filterRanks(
+        @RequestParam(required = false) List<String> track,
+        @RequestParam(required = false) List<String> environment,
+        @RequestParam(required = false) List<String> cost) {
+
+        FilterRequest filterRequest = new FilterRequest(track, environment, cost);
+        return rankService.getFilteredRanks(filterRequest);
+    }
+}
