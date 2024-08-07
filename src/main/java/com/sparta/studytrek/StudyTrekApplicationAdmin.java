056f8fbc6246ee038eb3b14919d8b75fb68086ae
DooDoo <160572286+Mua2@users.noreply.github.com>
2024-08-07 23:09:47 +0900

    🩹 fix: #36 포트8081 8080 관리자 권한 나누기

---
+++ src/main/java/com/sparta/studytrek/StudyTrekApplicationAdmin.java
@@ -0,0 +1,12 @@
+package com.sparta.studytrek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTrekApplicationAdmin {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTrekApplicationAdmin.class, args);
+	}
+}
