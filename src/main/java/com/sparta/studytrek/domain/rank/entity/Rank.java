938c8e34ea46f6219e84a3f2295e4ffedf9e2554
gminnimk <tvt1245@naver.com>
2024-08-08 12:22:19 +0900

    🩹 fix : #77 주석 해제 & recruitment 연관 관계 삭제

--- src/main/java/com/sparta/studytrek/domain/rank/entity/Rank.java
+++ src/main/java/com/sparta/studytrek/domain/rank/entity/Rank.java
@@ -1,35 +1,29 @@
-//package com.sparta.studytrek.domain.rank.entity;
-//
-//import com.sparta.studytrek.domain.camp.entity.Camp;
-//import com.sparta.studytrek.domain.recruitment.entity.Recruitment;
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//
-//@Entity
-//@Table(name = "rankings")
-//@Getter
-//@NoArgsConstructor
-//public class Rank {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "recruitment_id", nullable = false)
-//    private Recruitment recruitment;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "camp_id", nullable = false)
-//    private Camp camp;
-//
-//    @Column(nullable = false)
-//    private Integer ranking;
-//
-//    public Rank(Recruitment recruitment, Camp camp, Integer ranking) {
-//        this.recruitment = recruitment;
-//        this.camp = camp;
-//        this.ranking = ranking;
-//    }
-//}
+package com.sparta.studytrek.domain.rank.entity;
+
+import com.sparta.studytrek.domain.camp.entity.Camp;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "rankings")
+@Getter
+@NoArgsConstructor
+public class Rank {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "camp_id", nullable = false)
+    private Camp camp;
+
+    @Column(nullable = false)
+    private Integer ranking;
+
+    public Rank(Camp camp, Integer ranking) {
+        this.camp = camp;
+        this.ranking = ranking;
+    }
+}
