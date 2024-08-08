48e9338fab3700ad94e2a23ed8050352fcbb8919
gminnimk <tvt1245@naver.com>
2024-08-08 12:19:56 +0900

    💄 style : #77 주석 삭제

--- src/main/java/com/sparta/studytrek/domain/camp/dto/CampResponseDto.java
+++ src/main/java/com/sparta/studytrek/domain/camp/dto/CampResponseDto.java
@@ -1,20 +1,3 @@
-//package com.sparta.studytrek.domain.camp.dto;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//
-//@Getter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class CampResponseDto {
-//    private Long id;
-//    private String name;
-//    private String description;
-//    private String track;         // 추가된 필드
-//    private String environment;   // 추가된 필드
-//    private String cost;          // 추가된 필드
-//}
 package com.sparta.studytrek.domain.camp.dto;
 
 public record CampResponseDto(Long id, String name, String description) {}
