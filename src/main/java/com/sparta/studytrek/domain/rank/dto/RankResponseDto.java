cbd124adfbe612e0b3ac9491b12267334286d202
gminnimk <tvt1245@naver.com>
2024-08-08 12:27:24 +0900

    🩹 fix : #77 순위 정보 전송 객체

--- src/main/java/com/sparta/studytrek/domain/rank/dto/RankResponseDto.java
+++ src/main/java/com/sparta/studytrek/domain/rank/dto/RankResponseDto.java
@@ -1,20 +1,3 @@
-//package com.sparta.studytrek.domain.rank.dto;
-//
-//import com.sparta.studytrek.domain.recruitment.dto.RecruitmentResponseDto;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//
-//@Getter
-//@NoArgsConstructor
-//public class RankResponseDto {
-//
-//    private Long id;
-//    private Integer ranking;
-//    private RecruitmentResponseDto recruitment;
-//
-//    public RankResponseDto(Long id, Integer ranking, RecruitmentResponseDto recruitment) {
-//        this.id = id;
-//        this.ranking = ranking;
-//        this.recruitment = recruitment;
-//    }
-//}
+package com.sparta.studytrek.domain.rank.dto;
+
+public record RankResponseDto(Long id, Long campId, String campName, Integer ranking) {}
