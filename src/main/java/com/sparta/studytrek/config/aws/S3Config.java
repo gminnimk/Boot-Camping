cd8158a67cef96bc32f1a06e5fc6b2884101bcb2
gminnimk <tvt1245@naver.com>
2024-08-13 00:22:36 +0900

    💄 style : #82 AWS S3 클라이언트를 설정하고 생성하는 S3Config 클래스 주석 추가

--- src/main/java/com/sparta/studytrek/config/aws/S3Config.java
+++ src/main/java/com/sparta/studytrek/config/aws/S3Config.java
@@ -12,20 +12,22 @@ import org.springframework.context.annotation.Configuration;
 public class S3Config {
 
     @Value("${cloud.aws.region.static}")
-    private String region;
+    private String region;  // 애플리케이션 설정 파일에서 AWS 리전(region) 값을 주입받음
 
     @Value("${cloud.aws.credentials.accessKey}")
-    private String accessKey;
+    private String accessKey;  // AWS 액세스 키를 설정 파일에서 주입받음, AWS API 호출을 인증하는 데 사용
 
     @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
 
-    @Bean
+    @Bean  // 이 메서드가 반환하는 객체(AmazonS3)가 스프링 컨텍스트에서 관리되는 빈(Bean)으로 등록
     public AmazonS3 amazonS3Client() {
         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        // AmazonS3ClientBuilder를 사용하여 AmazonS3 클라이언트를 생성
+        // withCredentials 메서드를 사용하여 인증 정보를 설정하고, withRegion 메서드를 사용하여 S3 서비스가 사용할 리전을 설정
         return AmazonS3ClientBuilder.standard()
-            .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-            .withRegion(region)
-            .build();
+            .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))  // 인증 정보 설정
+            .withRegion(region)  // 리전 설정
+            .build();  // AmazonS3 객체를 생성하여 반환
     }
 }
