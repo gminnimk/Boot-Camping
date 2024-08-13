2eb2fc669dc8cad090b3188a1ddc77ad296937da
gminnimk <tvt1245@naver.com>
2024-08-13 14:52:56 +0900

    ✨ update : #77 캠프에 대한 순위를 찾기 위한 메서드 추가

--- src/main/java/com/sparta/studytrek/domain/rank/repository/RankRepository.java
+++ src/main/java/com/sparta/studytrek/domain/rank/repository/RankRepository.java
@@ -1,5 +1,6 @@
 package com.sparta.studytrek.domain.rank.repository;
 
+import com.sparta.studytrek.domain.camp.entity.Camp;
 import com.sparta.studytrek.domain.rank.entity.Rank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,5 @@ import java.util.Optional;
 @Repository
 public interface RankRepository extends JpaRepository<Rank, Long>, RankRepositoryCustom {
     Optional<Integer> findMaxRanking();
+    Optional<Rank> findByCamp(Camp camp);
 }
